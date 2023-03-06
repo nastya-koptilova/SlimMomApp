2cae8e26b63d66c72be40a7716ec9e96f719d483
TatianaSherstyuk2022 <grinkevichtatiana@gmail.com>
2023-03-06 21:48:26 +0200

    added caloriesForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 // import { Link } from 'react-router-dom';
 // import scss from '../styles/base/btn.scss';
-import { Btn } from './Btn/Btn';
+// import { Btn } from './Btn/Btn';
+
+import { HomePage } from 'pages/MainPage/MainPage';
 
 export const App = () => {
-
   return (
-   <>
-  
-   </>
+    <>
+      <HomePage />
+    </>
   );
 };
