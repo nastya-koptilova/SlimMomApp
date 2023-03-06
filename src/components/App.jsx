aed275a2ea51808b9ccb50d5b8a1e33439f8f97f
KaterynaChernetska <vesnavved@gmail.com>
2023-03-06 17:51:03 +0200

    Add styles and btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,7 @@
-// import { Link } from 'react-router-dom';
-// import scss from '../styles/base/btn.scss';
-import { Btn } from './Btn/Btn';
-
 export const App = () => {
 
   return (
-   <>
-  
+   <>  
    </>
   );
 };
