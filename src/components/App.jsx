95074c4f6fd7fca7ef6a11bd69de34d7b590425a
Ann <ann.mushka@gmail.com>
2023-03-07 13:54:58 +0200

    diary comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
+import { DairyPage } from 'pages/DairyPage/DairyPage';
 import { HomePage } from 'pages/MainPage/MainPage';
 
 export const App = () => {
   return (
     <>
       <HomePage />
+      <DairyPage />
     </>
   );
 };
