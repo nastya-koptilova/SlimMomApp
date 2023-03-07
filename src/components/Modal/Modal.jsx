b27a92f0bfa3fd8ae0125331a3ca156fb5cf77e1
KaterynaChernetska <vesnavved@gmail.com>
2023-03-07 16:43:06 +0200

    Update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,10 +3,33 @@ import { Link, useLocation } from 'react-router-dom';
 import { Btn } from 'components/Btn/Btn';
 import { createPortal } from 'react-dom';
 import { BsArrowReturnLeft } from 'react-icons/bs';
+import {useEffect} from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
-const Modal = () => {
+const Modal = ({onClose}) => {
+
+    const handleBackdropClick = event => {
+        if (event.currentTarget === event.target) {
+          onClose();
+        }
+      };
+    
+      useEffect(() => {
+    
+        const onModalClose = event => {
+          if (event.code === 'Enter') {
+            onClose();
+          }
+        };
+        
+        window.addEventListener('keydown', onModalClose);
+    
+        return () => {
+          window.removeEventListener('keydown', onModalClose);
+        };
+      }, [onClose]);
+    
   const location = useLocation();
 
   const handleBtnClick = () => {
@@ -15,7 +38,7 @@ const Modal = () => {
 
   return createPortal(
     <>
-      <div className={scss.overlay}>
+      <div className={scss.overlay} onClick={handleBackdropClick}>
         <Link to={location.state?.from || location}>
           <BsArrowReturnLeft className={scss.icon} />
         </Link>
@@ -29,9 +52,9 @@ const Modal = () => {
             <li ></li>
           </ul>
 
-          <Btn className={scss.btn}onBtnClick={handleBtnClick} type="button" variant="start">
+         <Link to='/register'><Btn className={scss.btn}onBtnClick={handleBtnClick} type="button" variant="start">
             Почніть худнути
-          </Btn>
+          </Btn></Link> 
         </div>
       </div>
     </>,
