b84b6a90d54224ac1a7af44534625ac878bcbc9d
KaterynaChernetska <vesnavved@gmail.com>
2023-03-07 15:42:44 +0200

    Add changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
