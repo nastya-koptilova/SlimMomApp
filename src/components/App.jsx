f576a96ce0280ecb6b4f436f189b6ef1e80ee1df
EllaHonchar <kuchincus82@gmail.com>
2023-03-07 16:37:02 +0200

    burger-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,24 @@
 import HomePage from 'pages/MainPage/MainPage';
 import { Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
 
 export const App = () => {
   return (
-    <> 
-     <Suspense fallback={<div>Loading</div>}>
-    <Routes>
-      {/* <Route path="/" element={<Layout />}> */}
-        <Route index element={<HomePage />} />
-        {/* <Route path="register" element={<RegistrationPage />} />
+    <>
+      <Suspense fallback={<div>Loading</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            {/* <Route path="register" element={<RegistrationPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="contacts" element={<DairyPage />} />
         <Route path="contacts" element={<CalculatorPage />} /> */}
-      {/* </Route> */}
+          </Route>
 
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  </Suspense>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
