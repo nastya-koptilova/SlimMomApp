1c4508237f64ec0998b075d09b9aadba66cd30c9
KaterynaChernetska <vesnavved@gmail.com>
2023-03-07 16:48:57 +0200

    Update modal

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,11 @@
 import Modal from 'components/Modal/Modal';
 import { DailyCaloriesForm } from 'components/DailyCaloriesForm/DailyCaloriesForm';
 import styles from './MainPage.module.css';
+import {useState} from 'react';
 
 function HomePage() {
+const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
     <div className={styles.homePage}>
       <DailyCaloriesForm />
