56c3d5f3655997187fb805ed85bafcfb7d65f57a
EllaHonchar <kuchincus82@gmail.com>
2023-03-07 09:05:37 +0200

    layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import { HomePage } from 'pages/MainPage/MainPage';
+import { Layout } from './Layout/Layout';
 
 export const App = () => {
   return (
     <>
+      <Layout />
       <HomePage />
     </>
   );
