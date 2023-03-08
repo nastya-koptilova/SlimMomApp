266b14ea2b007edd3a5897cbae3340bb55c747ba
Oleksandra Tiankina <ag7756862@gmail.com>
2023-03-08 14:41:28 +0200

    feat: add routes, logic add product

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,60 @@
-import { DairyPage } from 'pages/DairyPage/DairyPage';
+
+import React from 'react';
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { PrivateRoute } from 'redux/routes/PrivateRoute';
+import { PublicRoute } from 'redux/routes/RestrictedRoute';
+import { useAuth } from 'hooks/useAuth';
+import { refreshUser } from 'redux/authorization/authOperations';
+
 import LoginPage from 'pages/LoginPage/LoginPage';
 import HomePage from 'pages/MainPage/MainPage';
 import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
 import { Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
+import { DairyPage } from 'pages/DairyPage/DairyPage';
+import { DiaryAddProductForm } from './DiaryAddProductForm/DiaryAddProductForm';
+
+// const DairyPage = lazy(() => import('pages/DairyPage/DairyPage'));
 
 export const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const { isRefreshing, isLoggedIn } = useAuth();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch, isLoggedIn]);
+
+  return !!isRefreshing ? (
+    <b>Оновлення користувача...</b>
+  ) : (
     <>
       <Suspense fallback={<div>Loading</div>}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
-            <Route path="/register" element={<RegistrationPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dairy" element={<DairyPage />} />
-            {/*
-        <Route path="calculator" element={<CalculatorPage />} /> */}
+
+            <Route
+              path="/login" element={<PublicRoute redirectTo="/diary"
+                component={<Suspense fallback={<div>Loading</div>}><LoginPage />
+                </Suspense>} />}
+            />
+            <Route path="/register" element={<PublicRoute
+              redirectTo="/diary"
+              component={<Suspense fallback={<div>Loading</div>}>
+                <RegistrationPage /></Suspense>} />}
+            />
+
           </Route>
+          <Route path="/diary" element={
+            <Suspense fallback={<div>Loading</div>}>
+              <DairyPage></DairyPage>
+            </Suspense>} />
+
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
     </>
   );
 };
+
