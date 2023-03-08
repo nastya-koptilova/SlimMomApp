6a752ec27dbb873c01d26790f07899b35f1a9e9e
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-03-08 23:02:35 +0200

    fix slice

--- src/redux/authorization/authSelectors.js
+++ src/redux/authorization/authSelectors.js
@@ -1,6 +1,7 @@
-export const selectIsLoggedIn = state => state.userData.isLoggedIn;
+export const selectStatus = state => state.userData.status;
 export const selectToken = state => state.userData.token;
 export const selectUserName = state => state.userData.userName;
 export const selectUserEmail = state => state.userData.userEmail;
 export const selectUserId = state => state.userData.userId;
-export const selectPendingUserData = state => state.userData.pendingUserData;
+export const selectRefreshToken = state => state.userData.refreshToken;
+export const selectSid = state => state.userData.sid;
