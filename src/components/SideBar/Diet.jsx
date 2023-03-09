b631fc521dd8b3bb631f48248a4e644a704c7714
Oleksandra Tiankina <ag7756862@gmail.com>
2023-03-09 20:40:44 +0200

    feat: add info side

--- src/components/SideBar/Diet.jsx
+++ src/components/SideBar/Diet.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { nanoid } from 'nanoid';
 import s from './Sidebar.module.css';
+import { useSelector } from 'react-redux';
+import { selectUserInfo } from '../../redux/dailyCalories/caloriesSelectors';
 
-export const Diet = ({ diet }) => {
+export const Diet = () => {
+    const user = useSelector(selectUserInfo);
+    const diet = user.notAllowedProducts;
+
     function isGetDiet() {
         if (diet === undefined || diet.length === 0) return false;
         return true;
+    };
+
+    function ucFirst(str) {
+        if (!str) return str;
+        return str[0].toUpperCase() + str.slice(1);
     };
     return (
         <section>
@@ -13,11 +23,17 @@ export const Diet = ({ diet }) => {
             {!isGetDiet() && (<p className={s.SidebarText}>Ваша дієта буде відображатися тут</p>)}
             {isGetDiet() &&
                 (<ul className={s.SidebarList}>
-                    {diet.map((product) => (
-                        <li className={s.SidebarItem} key={nanoid()}>
-                            <p className={s.SidebarText}>product</p>
-                        </li>
-                    ))}
+                    {diet.map((product, index) => {
+                        if (index < 4) {
+                            return (
+                                <li className={s.SidebarItem} key={nanoid()}>
+                                    <p className={s.SidebarText}>{product}</p>
+                                </li>
+                            );
+                        } else {
+                            return null;
+                        }
+                    })}
                 </ul>)}
         </section>)
 }
