f270cec599e98b43c6eb666277ab219fb5a87b03
Oleksandra Tiankina <ag7756862@gmail.com>
2023-03-09 15:51:54 +0200

    fix: return diary func

--- src/pages/DairyPage/DairyPage.jsx
+++ src/pages/DairyPage/DairyPage.jsx
@@ -1,8 +1,19 @@
 import { DiaryAddProductForm } from 'components/DiaryAddProductForm/DiaryAddProductForm';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { DiaryProductsList } from 'components/DiaryProductList/DiaryProductList';
-
+import { SelectDate } from 'redux/diary/diarySelectors';
+import { getInfoOper } from 'redux/diary/diaryOperation';
 
 export function DairyPage() {
+  const date = useSelector(SelectDate);
+  const dispatch = useDispatch();
+  // const date = useSelector(selectDate);
+
+
+  useEffect(() => {
+    if (date) dispatch(getInfoOper(date));
+  }, [date, dispatch]);
 
   return (
     <>
