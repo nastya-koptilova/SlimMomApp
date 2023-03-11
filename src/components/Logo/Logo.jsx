018a7544a33b54f0df6cd609b996db965b31c23b
EllaHonchar <kuchincus82@gmail.com>
2023-03-11 12:43:44 +0200

    fixed logo - component/userInfo.style

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useWindowSize } from 'react-use';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/authorization/authSelectors';
 
 import logoPC from '../../images/logo/logoPC.svg';
 import logotab from '../../images/logo/logotab.svg';
@@ -8,12 +10,29 @@ import logoMob from '../../images/logo/logoMob.svg';
 export const Logo = () => {
   const { width } = useWindowSize();
 
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  // return (
+  //   <Link to="/">
+  //     <img
+  //       src={width > 1279 ? logoPC : logotab && width < 768 ? logoMob : logotab}
+  //       alt="SlimMom"
+  //     />
+  //   </Link>
+  // );
+
   return (
     <Link to="/">
-      <img
-        src={width > 1279 ? logoPC : logotab && width < 768 ? logoMob : logotab}
-        alt="SlimMom"
-      />
+      {isLoggedIn ? (
+        <img src={width > 1279 ? logoPC : logotab} alt="SlimMom" />
+      ) : (
+        <img
+          src={
+            width > 1279 ? logoPC : logotab && width < 768 ? logoMob : logotab
+          }
+          alt="SlimMom"
+        />
+      )}
     </Link>
   );
 };
