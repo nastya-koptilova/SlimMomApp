845f58b4b48833b4264f81e91c22288780af26c9
KaterynaChernetska <116261804+KaterynaChernetska@users.noreply.github.com>
2023-03-11 22:28:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-    {/* basename="/SlimMom" */}
+    <BrowserRouter basename="/SlimMom">    
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
