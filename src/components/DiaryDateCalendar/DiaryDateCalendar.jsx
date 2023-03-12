e46e70c88c9b941a623a13c1d2af1816620ed87a
TatianaSherstyuk2022 <grinkevichtatiana@gmail.com>
2023-03-12 13:55:05 +0200

    bagfix style

--- src/components/DiaryDateCalendar/DiaryDateCalendar.jsx
+++ src/components/DiaryDateCalendar/DiaryDateCalendar.jsx
@@ -1,14 +1,13 @@
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import React from 'react';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { BrowserInput } from './BrowserInput';
 import { useDispatch } from 'react-redux';
-import dayjs from 'dayjs';
-// import s from '../DiaryAddProductForm/DiaryAddProductForm.module.scss';
-import s from './DiaryDataCalendar.module.scss';
 import { getUserInfo } from 'redux/authorization/authOperations';
 import { setDate } from 'redux/diary/diarySlice';
+import dayjs from 'dayjs';
 import moment from 'moment';
+
+import s from './DiaryDataCalendar.module.scss';
 
 const DiaryDateCalendar = () => {
   const dispatch = useDispatch();
@@ -18,7 +17,6 @@ const DiaryDateCalendar = () => {
     dispatch(getUserInfo());
   };
 
-  
   const getCurrentDate = () => {
     const dateObj = new Date();
     const year = dateObj.getFullYear();
@@ -33,10 +31,11 @@ const DiaryDateCalendar = () => {
         className={s.inputCalendar}
         format="DD.MM.YYYY"
         defaultValue={dayjs(getCurrentDate())}
-        label="Custom input"
-        slots={{
-          textField: BrowserInput,
-        }}
+        // label="Custom input"
+        // slots={{
+        //   textField: BrowserInput,
+        // }}
+        style={{ fontSize: '26px' }}
         onChange={handleChangeDate}
       />
     </LocalizationProvider>
